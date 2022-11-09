5d0449964900ab021ac87ae5c4f4e5e49b35d5f0
gutierrezMartinIvan <martin192012@gmail.com>
2022-11-09 16:28:12 -0300

    done practicing with person class

--- app/src/main/java/ar/beniko/kotlinbasics/oop/Person.kt
+++ app/src/main/java/ar/beniko/kotlinbasics/oop/Person.kt
@@ -1,14 +1,33 @@
 package ar.beniko.kotlinbasics.oop
 
-fun main(){
-    var person1 = Person("Beniko", "Beniko")
-    var person2 = Person()
-    var person3 = Person(lastName = "Killua")
+fun main() {
+    var person1 = Person("Beniko", "Beniko", 324)
+    //var person2 = Person()
+    //var person3 = Person(lastName = "Killua")
+    println(person1)
 }
 
-class Person (firstName: String? = null, lastName: String = "Values"){
+ class Person(firstName: String? = null, lastName: String = "Values") {
+    private var hobby: String? = null
+    private var age: Int? = null
+    private var firstName: String? = firstName
+    private var lastName: String? = lastName
+
     //Initializer block
     init {
-        println("$firstName $lastName")
+
+    }
+
+    // Secondary constructor
+    constructor(firstName: String?, lastName: String, age: Int) : this(firstName, lastName) {
+        this.age = age
+    }
+
+    fun getHobby(): String? {
+        return hobby
+    }
+
+    override fun toString(): String {
+        return "Person(hobby=$hobby, age=$age, firstName=$firstName, lastName=$lastName)"
     }
 }
